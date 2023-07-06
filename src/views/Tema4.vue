18a3ada73a4909128f9dcb23e1d3e86ac46acc0c
elrilacho <camilobr17@gmail.com>
2023-07-06 15:16:53 -0500

    tema4_listo

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -5,7 +5,75 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 4
-      h1 
+      h1 Modelos pedagógicos y enfoques didácticos
+    .row.justify-content-center.mb-5
+      .col-lg-7.mb-lg-0.mb-3
+        p Para el desarrollo de este tema se abordará de manera inicial el concepto de #[b “modelo”], que según Flórez (1994) es #[b “aquella representación definidora de un fenómeno social, el cual se estudia con base a la interiorización y entendimiento del mismo”]; en este sentido, el concepto de modelo se puede referir como aquella acción teórica aproximada hacia la comprensión de la fisonomía de un fenómeno.
+          br
+          br
+          |Ahora bien, el autor Blanco (2002) en su texto denominado #[b “Los modelos pedagógicos”] encuentra el concepto e importancia de modelo, como aquel plan constituyente, integral e integrador de un fenómeno, es decir, desde la teoría y la práctica como aquel fundamento contemplativo en referencia de las posibles explicaciones a través de las implicaciones, los alcances, los paradigmas, entre otros aspectos que permitan el entendimiento de la fenomenología del estudio.
+      .col-lg-4.col-8: img(src='@/assets/curso/temas/tema4/1.png', alt='')
+    .cajon.color-acento-contenido.p-4.mb-4
+      p.mb-0 Acorde con lo anterior, se define modelo pedagógico como aquella fenomenología de la educación que basa su esfuerzo en la comprensión de los diferentes teóricos y padres de la invención educativa y de sus diferentes contrastes nacidos a través del tiempo. A continuación se explican de manera general algunos modelos pedagógicos, para que posteriormente elija el modelo a utilizar para el desarrollo de sus clases cuando sea el momento:
+    .row.justify-content-center.mb-5
+      .col-lg-10
+        AcordionA.mb-5(tipo="b" clase-tarjeta="tarjeta tarjeta--amarilla")
+          .row(titulo="Modelo tradicional")
+            .col-md-8.mb-4.mb-md-0
+              p Este modelo pedagógico también es conocido como el modelo externalista o escuela pasiva, centra el aprendizaje en el individuo de manera pasiva, es decir, fuentes externas generan el conocimiento del ser, por ejemplo, los maestros, los padres, el medio, entre otros. 
+                br
+                br
+                |Asimismo, este modelo maneja un enfoque conductista, en el que se considera que el maestro es la figura retórica que debe centrar sus esfuerzos en la creación de material para que el estudiante como objeto pasivo, interiorice y reproduzca el conocimiento.
+            .col-md-4
+              figure
+                img(src='@/assets/curso/temas/tema4/2.png', alt='')
+          .row(titulo="Modelo romántico")
+            .col-md-8.mb-4.mb-md-0
+              p Se proponen varios factores para el aprendizaje (maestro, estudiante y entorno pedagógico), el entorno y/o ambiente pedagógico adecuado, sencillo y flexible, el estudiante como individuo libre de denotar y experimentar las habilidades o talentos y el maestro como aquel sujeto dinamizador del proceso. 
+                br
+                br
+                |Este modelo propone un estímulo que permite en el individuo la experimentación directa con la creatividad, la sensibilidad y la capacidad, permitiendo un desarrollo integral en la formación de habilidades y talentos.
+
+            .col-md-4
+              figure
+                img(src='@/assets/curso/temas/tema4/3.png', alt='')
+          .row(titulo="Modelo progresista")
+            .col-md-8.mb-4.mb-md-0
+              p Obtiene sus principios bajo ideas filosóficas encaminadas al pragmatismo, uno de los teóricos de este modelo es Rodríguez, S. quien expone que el progresismo pedagógico es el aseguramiento de la sociedad respecto a su propio desarrollo.
+                br
+                br
+                |Este modelo apuesta por la creatividad e intereses de la persona para desarrollar y fortalecer la habilidades que posee.
+
+            .col-md-4
+              figure
+                img(src='@/assets/curso/temas/tema4/4.png', alt='')
+          .row(titulo="Modelo constructivista")
+            .col-md-8.mb-4.mb-md-0
+              p Bajo este modelo pedagógico se utilizan elementos que ayudan al estudiante a construir la respuesta a los problemas planteados por el docente y la sociedad, con el fin de que una vez finalizado el proceso se cree el aprendizaje. 
+                br
+                br
+                |Por lo que la idea propuesta en este modelo es comprender la enseñanza como un proceso dinámico, participativo, en donde el individuo debe participar activamente y no ser un simple receptáculo del saber.
+            .col-md-4
+              figure
+                img(src='@/assets/curso/temas/tema4/5.png', alt='')
+          .row(titulo="Modelo cognitivo - social ")
+            .col-md-8.mb-4.mb-md-0
+              p Modelo propuesto principalmente por psicólogos entre ellos Gagné, quienes centraron el aprendizaje en un compendio multifacético de capacidades, las cuales se ven influidas por la sociedad, la colectividad y los entornos.
+                br
+                br
+                |Según estos autores los entornos son espacios propicios y adecuados que ayudan y estimulan el aprendizaje, ahora bien, la sociedad y la colectividad lo que propician y estimulan es la crítica mutua y reforzamiento de producción grupal para la solución de problemas comunitarios. 
+            .col-md-4
+              figure
+                img(src='@/assets/curso/temas/tema4/6.png', alt='')
+          .row(titulo="Modelo contextual ")
+            .col-md-8.mb-4.mb-md-0
+              p También conocido como modelo de interaprendizaje, se define como aquel modelo pedagógico que se centra en la vida y el contexto, es decir, no se aprende dentro del aula, sino por el contrario a partir de contextos y ambientes externos generadores de problemas, los cuales propician en los estudiantes la búsqueda de alternativas de solución a través de la ciencia. 
+                br
+                br
+                |Su principal referente es el Psicólogo Vygotsky con su teoría del aprendizaje denominada “zona de desarrollo próximo o potencial”.
+            .col-md-4
+              figure
+                img(src='@/assets/curso/temas/tema4/7.png', alt='')
 
 </template>
 
@@ -26,4 +94,9 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.cajon.color-acento-contenido
+  background-color: #E5E9EA
+  &::before
+    background-color: #9AA8AD
+</style>
