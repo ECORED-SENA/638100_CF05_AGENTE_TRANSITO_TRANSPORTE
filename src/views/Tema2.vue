460870a1dd7ae0d0f2f7ccdc5be14cb5b95230cb
elrilacho <camilobr17@gmail.com>
2023-07-06 14:48:27 -0500

    tema4_listo

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -5,8 +5,44 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 2
-      h1 Titulo de primer nivel
-
+      h1 Fundamentos pedagógicos en seguridad vial
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-9.mb-lg-0.mb-3
+        p Teniendo en cuenta lo que menciona el Ministerio de Educación Nacional, la pedagogía se define como el saber de los docentes para orientar los procesos de formación de las personas, especialmente de niños y jóvenes, donde el docente genera una relación personal sobre lo que acontece diariamente en el trabajo con los alumnos, las alumnas y los colegas, para lograr el desarrollo humano y la construcción del conocimiento en ellos. 
+          br
+          br
+          |Los fundamentos pedagógicos se definen como #[b “todos aquellos principios y conocimientos del área de la pedagogía, que permitirán al docente conocer los métodos de enseñanza y aprendizaje que podrá utilizar durante su labor como pedagogo”] ( IMEI , 2019), por lo cual son la base de la educación donde el formador es quien propicia los espacios de análisis, reflexión y práctica.
+        .row.justify-content-center
+          .col-lg-4.mb-lg-0.mb-3
+            .tarjeta-avatar
+              img(src='@/assets/curso/temas/tema1/4.svg' alt='AvatarTop' style="width: 99px; bottom: 50px").m-auto
+              .tarjeta.color-primario.w-100
+                .p-4
+                  h4.text-center Guardia de trafico
+          .col-lg-4.mb-lg-0.mb-3
+            .tarjeta-avatar
+              img(src='@/assets/curso/temas/tema1/5.svg' alt='AvatarTop' style="width: 99px; bottom: 50px").m-auto
+              .tarjeta.color-primario.w-100
+                .p-4
+                  h4.text-center Lecciones sobre el trafico
+          .col-lg-4.mb-lg-0.mb-3
+            .tarjeta-avatar
+              img(src='@/assets/curso/temas/tema1/6.svg' alt='AvatarTop' style="width: 99px; bottom: 50px").m-auto
+              .tarjeta.color-primario.w-100
+                .p-4
+                  h4.text-center Enseñar sobre las señales de transito
+      .col-lg-3.col-6: img(src='@/assets/curso/temas/tema2/1.png', alt='')
+    .row.justify-content-center.mb-5.g-0
+      .col-lg-4.col-8: img(src='@/assets/curso/temas/tema2/2.png', alt='')
+      .col-lg-8
+        .p-5(style="background-color: #31313D").h-100
+          p.mb-0.text-white Ahora bien, los fundamentos pedagógicos en seguridad vial son los determinados por las normas de comportamiento vial y demás que regulan el tránsito y el transporte llevadas a ambientes educativos donde es el docente quien debe aterrizar estos temas bajo un contexto pedagógico en seguridad vial, reconociendo que el expositor o persona encargada de dar la orientación debe tener claro los conceptos a explicar y reconocer el público al cual se va a dirigir, ya que hablar de seguridad vial es un tema donde debe manejarse la realidad social y cultural de la movilidad del país. Las actividades pedagógicas que se pretendan realizar sobre este tema deben ser acordes a la etapa de la vida del grupo a ser capacitado, es por esto que se debe tener en cuenta lo establecido por el (Ministerio de Salud y Protección Social, 2022), frente al ciclo de vida que es un enfoque que permite entender las vulnerabilidades y las oportunidades de invertir durante etapas tempranas del desarrollo humano; reconoce que las experiencias se acumulan a lo largo de la vida y las conductas de una generación de actores viales repercutirán en otras personas, las cuales pueden estar en algún ciclo de la vida. 
+    .fondo.mn.p-5
+      p.mb-5 Por esta razón, orientar o educar sobre seguridad vial debe caracterizarse según el tipo de personas a capacitar, ya que ellas pueden estar en cualquiera de las etapas del desarrollo que se exponen a continuación:
+      img(src='@/assets/curso/temas/tema2/4.png', alt='Esquema gráfico que señala las etapas de desarrollo o ciclos etarios en que se pueden clasificar a las personas, en función de su formación en seguridad vial:<br>1 Primera infancia, de los 0 a los 5 años.<br>2 Infancia, de los 6 a los 11 años.<br>3 Adolescencia, de los 12 a los 18 años.<br>4 Juventud, de los 14 a los 26 años.<br>5, Adultez, de los 27 a los 59 años.<br>6, Personas mayores, de 60 o más años de edad.').mb-5
+      br
+      br
+    p Por lo anterior, el instructor para el caso SENA o docente para otras entidades que forme en seguridad vial debe estar preparado en las características que debe contener la planeación del aprendizaje en educación vial, como la prevención de la accidentalidad vial, la formación de hábitos y comportamientos seguros en la vía a cualquier tipo de público que le sea asignado como dicentes, utilizando las herramientas pedagógicas y didácticas necesarias para cada caso, dependiendo de la etapa en la que se encuentren.
 </template>
 
 <script>
@@ -26,4 +62,10 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.fondo
+  background-image: url(../assets/curso/temas/tema2/3.svg)
+  background-position: start
+  background-size:  100%
+  background-repeat: no-repeat
+</style>
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,7 +5,32 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Conceptos de andragogía y formación preescolar en seguridad vial
+    .bloque-texto-g.color-acento-contenido.p-3.p-sm-4.p-md-5.mb-5
+      .bloque-texto-g__img(
+        :style="{'background-image':`url(${require('@/assets/curso/temas/tema3/1.jpg')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 Los seres humanos se encuentran en un proceso constante de aprendizaje, desde el día de su nacimiento hasta el día de su muerte, entendiendo que nunca se para de aprender, todos los días hay una oportunidad de adquirir nuevos conocimientos. Para el caso específico de impartir formación sobre el control de movilidad, el transporte, la normatividad y la seguridad vial debe tenerse en cuenta, por ejemplo, que la educación de los niños, las niñas y los adolescentes debe ir enfocada a la enseñanza sobre el respeto de las normas de comportamiento vial y las obligaciones de cada uno de los actores viales, ya sea como conductor, pasajero o peatón, utilizando didácticas acordes a cada uno de los tipos de edades, utilizando estrategias didácticas activas y recursos didácticos como juegos en seguridad vial, impresos como cuentos y revistas infantiles, videos relacionados con el tema, auditivos como el reconocimiento de las señales sonoras de emergencia, equipos y herramientas como vehículos, bicicletas patinetas, patines, entre otros, que le permitan a los niños y los jóvenes asimilar los conceptos a través de todos sus sentidos.
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-9.mb-lg-0.mb-3
+        p: span(style="background-color: #FAE5B4 ") En cambio, la educación sobre seguridad vial para personas adultas debe generar recordación y para ello, es necesario mover las emociones de las personas, así que debe ir encaminada a dejarle un aprendizaje de por vida en un tema tan necesario para la sociedad como lo es la seguridad vial y los buenos hábitos en esta materia, ya que esto previene accidentes que pueden costar su vida.
+      .col-lg-3.col-5: img(src='@/assets/curso/temas/tema3/2.svg', alt='')
+    .tarjeta.p-5(style="background-color: #E5E9EA").mb-4
+      .row.justify-content-center.mb-5
+        .col-lg-10
+          p.mb-5 Aterrizando el tema en Colombia, es importante reconocer el aspecto poblacional y cultural de la zona donde posteriormente se aplique lo aprendido, es decir, se debe conocer la problemática general y las cifras de accidentalidad vial del municipio donde resida o donde se vaya a trabajar en este tipo de funciones, con el fin de lograr conectarlo con su día a día, por lo cual se presenta el siguiente dato del índice poblacional actual del país:
+          img(src='@/assets/curso/temas/tema3/3.svg', alt='Esquema gráfico del DANE que muestra los porcentajes del índice poblacional actual del país, así:<br>Actores viales hombres: 48,8%<br>Actores viales mujeres: 51,2%<br>Personas de 0 a 14 años: 22,6%<br>Personas de 15 a 65 años: 68,2%<br>Personas con más de 65 años: 9,1%')
+    figcaption.mb-5(style="background-color: #E1EAF4 ") Nota. Tomado de DANE (2021).
+    .tarjeta.card--2.p-5.pb-0
+      .row.justify-content-center.mb-5
+        .col-lg-7
+          p Un aforismo muy utilizado en el tema del civismo y comportamiento ciudadano según lo dijo Pitágoras 24 siglos atrás, en los cimientos de la sociedad, donde aún en la actualidad no es difícil encontrar en diferentes escenarios académicos y artísticos la siguiente frase “Educad a los niños y no será necesario castigar a los hombres”, siendo los niños la base de la próxima generación de actores viales quienes deben ser correctamente formados en el tema de la seguridad vial y los actores viales en calidad de adultos deben ser corregidos, y para esto hay actividades de prevención o aplicación de la norma como lo establece el Código de Tránsito al definir las posibles infracciones de tránsito. 
+            br
+            br
+            |Se puede concluir, entonces, que el docente o instructor en seguridad vial debe dominar las técnicas pedagógicas para la educación de los niños y los conceptos de andrología para la educación de los adultos, siendo el común denominador el tema de prevenir riesgos para evitar accidentes de tránsito y con ello, muertes.
+        .col-lg-4.col-6: img(src='@/assets/curso/temas/tema3/5.png', alt='')
+
 
 </template>
 
@@ -26,4 +51,10 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.card--2
+  background-image: url(../assets/curso/temas/tema3/fondo.png)
+  background-position: start
+  background-size:  cover
+  background-repeat: no-repeat
+</style>
